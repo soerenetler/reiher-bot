4a957336368bd2967cacf25f268de8297ed1cc13
soerenetler <soeren.etler@gmail.com>
2021-07-25 21:51:13 +0200

    move DBPersistence

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ from actions import generalActions, en_reiherbergActions, reiherbergActions
 from configparser import ConfigParser
 import argparse
 
-from mongo_persistence import DBPersistence
+from digital_guide.mongo_persistence import DBPersistence
 
 import os
 import sys
