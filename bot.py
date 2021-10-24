ebb9a4baa069cea641009dfe6c6e0afc1204af92
soerenetler <soeren.etler@gmail.com>
2021-10-24 21:32:42 +0200

    print webhook_url

--- bot.py
+++ bot.py
@@ -82,6 +82,8 @@ if __name__ == '__main__':
     dp.add_handler(conv_handler)
     dp.add_error_handler(error_handler)
 
+    print("webhook_url: ", os.environ.get("APP_URL") + TOKEN)
+
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=TOKEN,
