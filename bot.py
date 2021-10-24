a1d1024a952ae07617debcac6c78a20c8fd702c0
soerenetler <soeren.etler@gmail.com>
2021-10-24 23:24:50 +0200

    print token

--- bot.py
+++ bot.py
@@ -83,10 +83,11 @@ if __name__ == '__main__':
     dp.add_error_handler(error_handler)
 
     print("webhook_url: ", os.environ.get("APP_URL") + TOKEN)
+    print("PORT", PORT)
 
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
-                          url_path=TOKEN)
-    updater.bot.setWebhook(os.environ.get("APP_URL") + TOKEN)
+                          url_path=TOKEN,
+                          webhook_url = os.environ.get("APP_URL") + TOKEN)
 
-updater.idle()
+    updater.idle()
